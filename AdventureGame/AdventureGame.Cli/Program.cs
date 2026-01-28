0a5b10663a056fea044a1b2f1abb17f431b09fa4
Devin Holt <HOLTDW1@ETSU.EDU>
2026-01-28 17:03:36 -0500

    khbfv

---
+++ AdventureGame/AdventureGame.Cli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventureGame.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ AdventureGame/AdventureGame.Core/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventureGame.Core
+{
+    public class Class1
+    {
+        //I'm going to try to set up the tile system I want. Move later based on specified conventions
+
+            /* To begin, I want a 5x5 tile that has 3 doors, left, right, and center. 
+             The whole maze should be 15 across, with the each tile space being 5x5
+            
+            The starting tile should generate a bottom left, right, and middle tile.
+            The left wing should have more monsters and items, but less dead ends
+            The right wing will be safe(er), but not guaranteed to be passable
+            The middle will be a mix
+
+        When the player interacts with a door, generate a tile based on the current coordinates 
+            Example: if in the bottom left, create a middle left.
+            You can probably get soft-locked. So include a reset button
+             */
+
+        }
+    }
